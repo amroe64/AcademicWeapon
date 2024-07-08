d2141b1aa15eb4c5c13f5f4501280411c574dc0d
Amr Elamin <amrelamin04@gmail.com>
2024-07-08 18:40:16 -0400

    Push 1

---
+++ poor-ravens-allow/src/App.tsx
@@ -0,0 +1,60 @@
+import { GitHubBanner, Refine, WelcomePage } from '@refinedev/core';
+import { DevtoolsPanel, DevtoolsProvider } from '@refinedev/devtools';
+import { RefineKbar, RefineKbarProvider } from '@refinedev/kbar';
+
+import {
+	useNotificationProvider
+} from '@refinedev/antd';
+import '@refinedev/antd/dist/reset.css';
+
+import { ForgotPassword, Home, Login, Register } from "./pages";
+import { authProvider, dataProvider, liveProvider } from './providers';
+
+import routerBindings, {
+	DocumentTitleHandler,
+	UnsavedChangesNotifier,
+} from '@refinedev/react-router-v6';
+import { App as AntdApp } from 'antd';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+function App() {
+	return (
+		<BrowserRouter>
+			<GitHubBanner />
+			<RefineKbarProvider>
+				<AntdApp>
+					<DevtoolsProvider>
+						<Refine
+							dataProvider={dataProvider}
+							liveProvider={liveProvider}
+							notificationProvider={useNotificationProvider}
+							routerProvider={routerBindings}
+							authProvider={authProvider}
+							options={{
+								syncWithLocation: true,
+								warnWhenUnsavedChanges: true,
+								useNewQueryKeys: true,
+								projectId: 'vL3302-WQgnvU-pZSOVk',
+								liveMode: 'auto',
+							}}
+						>
+							<Routes>
+								<Route index element={<WelcomePage />} />
+								<Route index element={<Home />} />
+								<Route path="/register" element={<Register />} />
+								<Route path="/login" element={<Login />} />
+								<Route path="/forgot-password" element={<ForgotPassword />} />
+							</Routes>
+							<RefineKbar />
+							<UnsavedChangesNotifier />
+							<DocumentTitleHandler />
+						</Refine>
+						<DevtoolsPanel />
+					</DevtoolsProvider>
+				</AntdApp>
+			</RefineKbarProvider>
+		</BrowserRouter>
+	);
+}
+
+export default App;
